51fe324d38b2506380b3a5807adecf9c6c8d7ad8
payamaya <paulyashou81@gmail.com>
2022-09-21 00:13:37 +0200

    first commit single-page-portfolio

--- single-page-portfolio/src/components/Contact.js
+++ single-page-portfolio/src/components/Contact.js
@@ -5,18 +5,21 @@ const Contact = () => {
     scroll.scrollToTop()
   }
   return (
-    <div
-      id='contact'
-      style={{ width: '100vw', height: '50vh' }}
-    >
+    <div id='contact' style={{ width: '100vw', height: '50vh' }}>
       <h1>Contact</h1>
       <BsFillArrowUpCircleFill
         onClick={toTop}
-        style={{ margiLeft: '80rem',
-         marginTop:'30rem',
-         fontSize: '30px',
-          cursor: 'pointer' }}
+        style={{
+          margiLeft: '80rem',
+          marginTop: '30rem',
+          fontSize: '30px',
+          cursor: 'pointer',
+        }}
       />
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Expedita dolor
+        ipsa nisi a doloribus eligendi.
+      </p>
     </div>
   )
 }
